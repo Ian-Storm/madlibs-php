caae81614be8b528d78465fd9f4c248e11247ad9
Ian Storm <99039737@mydavinci.nl>
2017-05-10 13:17:40 +0200

    update

--- index_2.php
+++ index_2.php
@@ -18,7 +18,7 @@
 	<label>Wat is je favoriete getal? </label><input type="text" name="3"><br>
 	<label>Wat heb je altijd bij je als op vakantie gaat? </label><input type="text" name="4"><br>
 	<label>Wat is je beste persoonlijke eigenschap? </label><input type="text" name="5"><br>
-	<label>Wat is je slechte persoonlijke eigenschap? </label><input type="text" name="6"><br>
+	<label>Wat is je slechtste persoonlijke eigenschap? </label><input type="text" name="6"><br>
 	<label>Wat is het ergste wat je kan overkomen? </label><input type="text" name="7"><br>
 		<input type="submit" name="submit">
 </form>
